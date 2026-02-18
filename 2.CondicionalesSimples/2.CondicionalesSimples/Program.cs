965d605b4e2d8b0e356654b48c5a7b16ce735a59
Sebastian Nuñez <juan.nunezg@upb.edu.co>
2026-02-18 09:17:00 -0500

    Condicional simple

---
+++ 2.CondicionalesSimples/2.CondicionalesSimples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   string nombre = " ";
+               float sueldo = 0;
+               Console.WriteLine("Ingrese el nombre de usuario");
+               nombre = Console.ReadLine();//Todo lo que el usuario ingresa desde la consola llega como una cadena de crácteres
+
+               Console.WriteLine("Ingrese el valor del sueldo");
+               sueldo = Convert.ToSingle (Console.ReadLine() );
+
+
+               if (sueldo>=3000)
+               {
+                   Console.WriteLine($"Elusuario {nombre}, debe abonar impuestos");
+               }*/
+
+            float edad = 0;
+            Console.WriteLine("Ingrese edad del usuario");
+            edad = Convert.ToSingle(Console.ReadLine());
+
+            if (edad>=18)
+            {
+                Console.WriteLine($"Bienvenido a mi sitio web");
+            }
+
+        }
+    }
+}
---
+++ VariablesTiposDatosOperadores/VariablesTiposDatosOperadores/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace VariablesTiposDatosOperadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bienvenido al curso de fundamentos de programación");
+            Console.ReadKey();
+        }
+    }
+}
