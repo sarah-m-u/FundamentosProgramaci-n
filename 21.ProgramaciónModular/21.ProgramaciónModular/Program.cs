3bb4e02fc847e0067a7b440eaa3003bf17bf51b2
sarah-m-u <sarah.marquezu@upb.edu.co>
2026-04-24 09:41:16 -0500

    Add files via upload

---
+++ 21.ProgramaciónModular/21.ProgramaciónModular/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.ProgramaciónModular
+{
+    internal class Program
+    {
+        static string nombre = "pepe";
+        static string apellido = "pepe";
+        static void Main(string[] args)
+        {
+            //programación modular
+            //procedimientos sin parámetros
+            BorrarPantalla();
+            Saludo();
+            Saludo(nombre, apellido);
+            Console.ReadKey();
+            Console.WriteLine($"edad: {CalcularEdad()}");
+            Console.WriteLine($"Edad;{CalculadEdadAñoNaciomiento(2026, 1981)}");
+        }
+            static void BorrarPantalla()
+            {
+                Console.Clear();
+            }
+            static void Saludo()
+            {
+                string nombre = "Pepe";
+                Console.WriteLine($"Bienvenido a la clase de programación modular {nombre}");
+            }
+            //Procedimientos con parámetros 
+            static void Saludo (string nombre, string apellidos)
+            {
+
+                Console.WriteLine($"Bienvenido a la clase de programación modular {nombre}");
+            }
+            //funciones sin parámetros
+
+            static int CalcularEdad()
+            {
+                int añoActual = 2026;
+                int añoNacimiento = 1981;
+
+                int edad = añoActual - añoNacimiento;
+
+                return edad;
+            }
+            //funciones con parámetros
+            static int CalculadEdadAñoNaciomiento(int añoActual, int añoNacimiento)
+            {
+                return añoActual - añoNacimiento;
+            }
+
+        }
+    }
+
---
+++ 21.ProgramaciónModular/21.ProgramaciónModular/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("21.ProgramaciónModular")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("21.ProgramaciónModular")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("4edba1ce-8bc0-4348-8c8c-32bfc47399fd")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
